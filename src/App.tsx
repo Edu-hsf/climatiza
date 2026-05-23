9b91090f18714b8e9be7794a60a8eebe58aeb9f2
Eduardo Henrique <eduardo.h.fernandes.oficial@gmail.com>
2026-05-23 14:10:46 -0300

    fix: remoção de funções do redux removidas do App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import { useAppDispatch } from '@/hooks';
-import { changeLocationAsync } from '@/store/locationSlice';
-import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
 export default function App() {
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(changeLocationAsync());
-
-  }, [dispatch]);
-
   return (
     <div className='h-full mb-6'>
       <BrowserRouter>
-          <Routes />
+        <Routes />
       </BrowserRouter>
     </div>
   );
