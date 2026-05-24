79b1733fd4bd10c5824cdf4f43576085d7c33fbb
Eduardo Henrique <eduardo.h.fernandes.oficial@gmail.com>
2026-05-24 08:09:16 -0300

    fix: removido estado de erro no coordinatesContext

--- src/contexts/CoordinatesContext.tsx
+++ src/contexts/CoordinatesContext.tsx
@@ -1,35 +1,37 @@
 import { createContext, useEffect, useState } from 'react';
 
-const CoordinatesContext = createContext({});
-
 interface Coordinates {
     lat: number
     long: number
+}
+
+interface CoordinatesContextType {
+    coordinates: Coordinates | null
+    setCoordinates: React.Dispatch<React.SetStateAction<Coordinates | null>>
 }
 
 interface CoordinatesProviderProps {
     children: React.ReactNode
 }
 
+export const CoordinatesContext = createContext<CoordinatesContextType>({} as CoordinatesContextType);
+
 export function CoordinatesProvider({ children }: CoordinatesProviderProps) {
     const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
-    const [ error, setError ] = useState<boolean>(false);
 
     useEffect(() => {
-        if (!navigator.geolocation) {
-            return setError(true);
-        }
-
-        navigator.geolocation.getCurrentPosition((position) => {
-            setCoordinates({
-                lat: position.coords.latitude,
-                long: position.coords.longitude,
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                setCoordinates({
+                    lat: position.coords.latitude,
+                    long: position.coords.longitude,
+                });
             });
-        });
+        }
     }, []);
 
     return (
-        <CoordinatesContext.Provider value={{ coordinates, setCoordinates, error }}>
+        <CoordinatesContext.Provider value={{ coordinates, setCoordinates }}>
             {children}
         </CoordinatesContext.Provider>
     );
