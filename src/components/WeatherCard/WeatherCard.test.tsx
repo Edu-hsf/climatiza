e94818886e9bf344f61321d474a055c46f1a8bcc
Eduardo Henrique <eduardo.h.fernandes.oficial@gmail.com>
2026-05-24 18:34:31 -0300

    refactor: testes para WeatherCard refatorados

--- src/components/WeatherCard/WeatherCard.test.tsx
+++ src/components/WeatherCard/WeatherCard.test.tsx
@@ -1,23 +1,22 @@
 import { render, screen } from "@testing-library/react";
-import { Thermometer } from "lucide-react";
 import WeatherCard from '.';
 
 describe('WeatherCard', () => {
     it('deve retornar o icone corretamente', () => {
-        render(<WeatherCard icon={Thermometer} />);
+        render(<WeatherCard type="temperature" />);
 
         expect(screen.getByTestId('weather-icon')).toBeInTheDocument();
     });
 
     it('deve retornar a descrição corretamente', () => {
-        render(<WeatherCard description='Sensação térmica' />);
+        render(<WeatherCard type="temperature" />);
 
-        expect(screen.getByText('Sensação térmica')).toBeInTheDocument();
+        expect(screen.getByText('Sensação Térmica')).toBeInTheDocument();
     });
 
     it('deve retornar o dado climático corretamente', () => {
         render(<WeatherCard type='temperature' weatherData={30} />);
 
-        expect(screen.getByText('30')).toBeInTheDocument();
+        expect(screen.getByText('30°C')).toBeInTheDocument();
     });
 });
