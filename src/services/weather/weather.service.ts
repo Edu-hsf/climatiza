da681bda0e544d565b06864f7a4a91e4676a56fb
Eduardo Henrique <eduardo.h.fernandes.oficial@gmail.com>
2026-05-24 10:37:05 -0300

    fix: correção de tipagem dos parâmetros do getWeather

--- src/services/weather/weather.service.ts
+++ src/services/weather/weather.service.ts
@@ -4,12 +4,12 @@ import { weatherMap } from './weather.mapper';
 import type { WeatherAPI } from './weather.types';
 
 export default async function getWeather(
-    lat: string,
-    long: string,
+    lat: number,
+    long: number,
 ) {
     const params = new URLSearchParams({
-        latitude: lat,
-        longitude: long,
+        latitude: lat.toString(),
+        longitude: long.toString(),
 
         daily: [
             'temperature_2m_max',
