cc83dc1def7b1a6f4ba30904d1d1cb6b5fa6a20a
Eduardo Henrique <eduardo.h.fernandes.oficial@gmail.com>
2026-05-24 19:59:21 -0300

    refactor: componente WeatherCard refatorado

--- src/components/WeatherCard/index.tsx
+++ src/components/WeatherCard/index.tsx
@@ -1,19 +1,80 @@
-import type { LucideProps } from "lucide-react";
-import type { ComponentType } from "react";
-import { AspectRatio } from "radix-ui";
+import { cn } from "@/lib/utils";
+import { Card, CardContent } from "../ui/card";
+import { Droplet, Eye, Gauge, Sun, Thermometer, Wind, type LucideIcon } from "lucide-react";
+import { Skeleton } from "../ui/skeleton";
 
-interface WeatherCardsProps {
-    icon: ComponentType<LucideProps>
-    label: string
-    data: string
+interface WeatherCardsProps extends React.ComponentProps<"div"> {
+    type: 'temperature' | 'wind_speed' | 'humidity' | 'visibility' | 'pressure' | 'precipitation'
+    weatherData?: number | string
+    isLoading?: boolean
 }
 
-export default function WeatherCard ({icon: Icon, label, data}: WeatherCardsProps) {
+export default function WeatherCard({ type, weatherData, isLoading, ...props }: WeatherCardsProps) {
+    let Icon: LucideIcon | null = null;
+    let description: string | null = null;
+
+    switch (type) {
+        case 'temperature':
+            Icon = Thermometer;
+            description = 'Sensação Térmica';
+            weatherData = weatherData + '°C';
+            break;
+
+        case 'wind_speed':
+            Icon = Wind;
+            description = 'Velocidade do Vento';
+            weatherData = weatherData + ' km/h S';
+            break;
+
+        case 'humidity':
+            Icon = Droplet;
+            description = 'Humidade';
+            weatherData = weatherData + '%';
+            break;
+
+        case 'visibility':
+            Icon = Eye;
+            description = 'Visibilidade';
+            weatherData = weatherData + ' km';
+            break;
+
+        case 'pressure':
+            Icon = Gauge;
+            description = 'Pressão';
+            weatherData = weatherData + ' hPa';
+            break;
+
+        case 'precipitation':
+            Icon = Sun;
+            description = 'Precipitação';
+            weatherData = weatherData + '%';
+            break;
+
+        default:
+            break;
+    }
+
     return (
-        <AspectRatio>
-            <Icon size={28} className="mb-3"/>
-            <p className="text-muted-foreground text-sm mb-1">{label}</p>
-            <p className="text-xl">{data}</p>
-        </AspectRa>
+        <Card className={cn("hover:bg-white/15", props.className)} {...props}>
+            <CardContent>
+                {isLoading ?
+                    (
+                        <div className="space-y-3.5">
+                            <Skeleton className="w-7 h-7 rounded-full" />
+                            <Skeleton className="w-40 h-3.5" />
+                            <Skeleton className="w-20 h-7" />
+                        </div>
+                    ) : (
+                        <>
+                            {Icon && <Icon size={28} className="mb-3" data-testid='weather-icon' />}
+                            <p className="text-muted-foreground text-sm mb-1">{description}</p>
+                            <strong>
+                                <p className="text-foreground text-xl">{weatherData}</p>
+                            </strong>
+                        </>
+                    )
+                }
+            </CardContent>
+        </Card>
     );
 }
