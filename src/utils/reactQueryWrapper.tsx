023c2f001977c4fd2777c4d479707abadcacba53
Eduardo Henrique <eduardo.h.fernandes.oficial@gmail.com>
2026-05-24 08:40:30 -0300

    refactor: create wrapper com coordinates context
    - adicionados parametros de latitude e longitude
    - adicionado CoordinatesContext.Provider dentro de CoordinatesContext.Provider

--- src/utils/reactQueryWrapper.tsx
+++ src/utils/reactQueryWrapper.tsx
@@ -1,3 +1,4 @@
+import { CoordinatesContext } from '@/contexts/CoordinatesContext';
 import {
   QueryClient,
   QueryClientProvider,
@@ -13,7 +14,10 @@ export function createTestQueryClient() {
   });
 }
 
-export function createWrapper() {
+export function createWrapper(coordinates: { lat: number, long: number } | null = {
+  lat: 10,
+  long: -20,
+}) {
   const queryClient = createTestQueryClient();
 
   return function Wrapper({
@@ -23,7 +27,9 @@ export function createWrapper() {
   }) {
     return (
       <QueryClientProvider client={queryClient}>
-        {children}
+        <CoordinatesContext.Provider value={{ coordinates, setCoordinates: vi.fn() }}>
+          {children}
+        </CoordinatesContext.Provider>
       </QueryClientProvider>
     );
   };
