a53bc139596c565ebc879269cc23cdc3263df80f
Eduardo Henrique <eduardo.h.fernandes.oficial@gmail.com>
2026-05-24 19:30:22 -0300

    refactor: testes aprimorados para getWeather

--- src/services/weather/weather.test.ts
+++ src/services/weather/weather.test.ts
@@ -2,6 +2,7 @@ import getWeather from './weather.service';
 import { weatherMap } from './weather.mapper';
 import weatherFetch from './weather.client';
 import { weatherMock } from './weather.mock';
+import { Cloud } from 'lucide-react';
 
 vi.mock('./weather.client');
 
@@ -21,18 +22,40 @@ describe('weatherMapper', () => {
     const data = weatherMap(weatherMock);
 
     it('Deve formatar corretamente os dados atuais do clima', () => {
-        expect(data.current.time.toLocaleDateString('pt-BR')).toBe('17/05/2026');
-        expect(data.current.weatherCode).toBe(2);
-        expect(data.current.weatherDescription).toBe('Parcialmente nublado');
-        expect(data.current.isDay).toBe(true);
+        expect(data.current).toEqual({
+            time: expect.any(Date),
+            temperature: 25,
+            apparentTemperature: 26,
+            windSpeed: 12,
+            humidity: 70,
+            pressure: 1015,
+            precipitation: 0,
+            visibility: 10.0,
+            weatherCode: 2,
+            weatherDescription: 'Parcialmente nublado',
+            weatherIcon: Cloud,
+            isDay: true,
+        });
     });
 
     it('Deve formatar corretamente os dados climáticos por hora', () => {
-        expect(data.hourly[0].temperature).toBe(21);
+        expect(data.hourly[0]).toEqual({
+            time: expect.any(Date),
+            temperature: 21,
+            weatherCode: 2,
+            weatherDescription: 'Parcialmente nublado',
+            weatherIcon: Cloud,
+        });
     });
 
     it('Deve formatar corretamente os dados climáticos diários', () => {
-        expect(data.daily[0].temperatureMax).toBe(25);
-        expect(data.daily[0].temperatureMin).toBe(19);
+        expect(data.daily[0]).toEqual({
+            time: expect.any(Date),
+            temperatureMax: 25,
+            temperatureMin: 19,
+            weatherCode: 2,
+            weatherDescription: 'Parcialmente nublado',
+            weatherIcon: Cloud,
+        });
     });
 });
